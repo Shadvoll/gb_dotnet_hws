1fe229e19c576c2614d0651c3b2a8ed34858d8c7
shaukat <shadvoll@gmail.com>
2023-01-16 12:51:55 +0300

    Added 1st task.

---
+++ task1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача: на вход 2 числа. Вывести макс и мин
+Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+
+if (a > b){
+    Console.WriteLine("Максимальное: " + a);
+    Console.WriteLine("Минимальное: " + b);
+}
+else if (a < b) {
+    Console.WriteLine("Максимальное: " + b);
+    Console.WriteLine("Минимальное: " + a);
+}
+else{
+    Console.WriteLine("Оба числа равны");
+}
+
+//Через библ-ку Math
+// Console.WriteLine(Math.Max(a,b));
+// Console.WriteLine(Math.Min(a,b));
