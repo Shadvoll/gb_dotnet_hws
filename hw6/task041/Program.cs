2da101553be33d0456f4786133321b57978b3717
shaukat <shadvoll@gmail.com>
2023-01-31 12:01:52 +0300

    added hw6

---
+++ hw6/task041/Program.cs
@@ -0,0 +1,20 @@
+﻿// * Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//         0, 7, 8, -2, -2 -> 2
+//         1, -7, 567, 89, 223-> 3
+
+int GetNum(string text)
+{
+    Console.Write(text);
+    return int.Parse(Console.ReadLine());
+}
+
+int amount = GetNum("Введите число количества чисел: ");
+int count = 0;
+for (int i = 0; i < amount; i++)
+{
+    if (GetNum($"Введите {i} число: ") > 0)
+    {
+        count++;
+    }
+}
+Console.WriteLine($"Количество чисел больше нуля: {count}");
---
+++ hw6/task043/Program.cs
@@ -0,0 +1,28 @@
+﻿// * Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+//         b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+double GetNum(string text)
+{
+    Console.Write(text);
+    return double.Parse(Console.ReadLine());
+}
+
+Console.WriteLine("Программа, которая находит точку персечения двух прямых: ");
+Console.WriteLine("1. y = k1 * x + b1");
+Console.WriteLine("2. y = k2 * x + b2");
+double b1 = GetNum("Введите b1 = ");
+double k1 = GetNum("Введите k1 = ");
+double b2 = GetNum("Введите b2 = ");
+double k2 = GetNum("Введите k2 = ");
+double x0 = (b2 - b1) / (k1 - k2);
+double y0 = k1 * x0 + b1;
+if (Math.Abs(k1) == Math.Abs(k2))
+{
+    Console.WriteLine("Прямые параллельны. Решений нет.")
+}
+else
+{
+    Console.WriteLine($"b1 = {b1}, k1 = {k1}, b2 = {b2}. k2 = {k2} -> ({x0}; {y0})");
+}
